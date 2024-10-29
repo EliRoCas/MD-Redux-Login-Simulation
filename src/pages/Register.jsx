6bc9fe2638e37de6414513ef8427211bfe715c64
Eli RoCas <elir_94@hotmail.com>
2024-10-28 20:22:01 -0500

    add: register component and style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,51 @@
+import "./register.scss"
+
 function Register() {
+    // const handleSubmit = ($event) => {
+    //     $event.preventDefault();
+    // };
+
     return (
-        <div>Example Register</div>
+        <div className="register">
+            <form className="registerForm">
+                <h2>Sign Up</h2>
+                <div className="name">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" />
+                </div>
+                <div className="lastname">
+                    <label htmlFor="lastname">Lastname</label>
+                    <input type="text" id="lastname" name="lastname" />
+                </div>
+                <div className="documentType">
+                    <label htmlFor="documentType">Document Type</label>
+                    <select name="documentType" id="documentType">
+                        <option value="cc">Citizenship Card</option>
+                        <option value="ti">Identity Card</option>
+                        <option value="passport">Passport</option>
+                    </select>
+                </div>
+                <div className="documentNumber">
+                    <label htmlFor="documentNumber">Document Number</label>
+                    <input type="text" id="documentNumber" name="documentNumber" />
+                </div>
+                <div className="email">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div className="password">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <div className="confirmPassword">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input type="password" id="confirmPassword" name="confirmPassword" />
+                </div>
+
+                <button type="submit"> Sign Up</button>
+            </form>
+        </div>
     )
 }
 
-export default Register
+export default Register;
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,13 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
-// import Login from "../pages/Login";
 
 const PrivateRoute = ({ children }) => {
     const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
 
     return isLoggedIn ? children : <Navigate to="/login" />;
-    // return isLoggedIn ? children : <Login />;
 };
 console.log(PrivateRoute);
 
