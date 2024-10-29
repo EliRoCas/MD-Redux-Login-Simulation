3a1bc5092065c9bf0f48da948d463619b66e9596
Eli RoCas <elir_94@hotmail.com>
2024-10-29 10:21:52 -0500

    add userSlice and update register form data

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,51 +1,129 @@
-import "./register.scss"
+import { useDispatch } from "react-redux";
+import { addUser } from "../redux/slices/usersSlice";
+
+import { useState } from "react";
+
+import "./register.scss";
 
 function Register() {
-    // const handleSubmit = ($event) => {
-    //     $event.preventDefault();
-    // };
+  const dispatch = useDispatch();
 
-    return (
-        <div className="register">
-            <form className="registerForm">
-                <h2>Sign Up</h2>
-                <div className="name">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" />
-                </div>
-                <div className="lastname">
-                    <label htmlFor="lastname">Lastname</label>
-                    <input type="text" id="lastname" name="lastname" />
-                </div>
-                <div className="documentType">
-                    <label htmlFor="documentType">Document Type</label>
-                    <select name="documentType" id="documentType">
-                        <option value="cc">Citizenship Card</option>
-                        <option value="ti">Identity Card</option>
-                        <option value="passport">Passport</option>
-                    </select>
-                </div>
-                <div className="documentNumber">
-                    <label htmlFor="documentNumber">Document Number</label>
-                    <input type="text" id="documentNumber" name="documentNumber" />
-                </div>
-                <div className="email">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" />
-                </div>
-                <div className="password">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" />
-                </div>
-                <div className="confirmPassword">
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" id="confirmPassword" name="confirmPassword" />
-                </div>
+  const [formData, setFormData] = useState({
+    name: "",
+    lastname: "",
+    documentType: "",
+    documentNumber: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-                <button type="submit"> Sign Up</button>
-            </form>
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    dispatch(addUser(formData));
+
+    setFormData({
+      name: "",
+      lastname: "",
+      documentType: "",
+      documentNumber: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+
+    alert("User added successfully");
+  };
+
+  return (
+    <div className="register">
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <div className="name">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
         </div>
-    )
+        <div className="lastname">
+          <label htmlFor="lastname">Lastname</label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="documentType">
+          <label htmlFor="documentType">Document Type</label>
+          <select name="documentType" id="documentType">
+            <option value="cc">Citizenship Card</option>
+            <option value="ti">Identity Card</option>
+            <option value="passport">Passport</option>
+          </select>
+        </div>
+        <div className="documentNumber">
+          <label htmlFor="documentNumber">Document Number</label>
+          <input
+            type="text"
+            id="documentNumber"
+            name="documentNumber"
+            value={formData.documentNumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="email">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="password">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="confirmPassword">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit"> Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./slices/loginSlice";
+import usersReducer from "./slices/usersSlice";
 
 export const store = configureStore({
   reducer: {
     login: loginReducer,
+    users: usersReducer,
   },
 });
