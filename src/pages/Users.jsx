75f5cdf10626ca23551d8d680a4fc6cb582968c0
Eli RoCas <elir_94@hotmail.com>
2024-10-29 11:12:17 -0500

    add: registered users style

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeUser } from "../redux/slices/usersSlice";
+
+import "./users.scss";
+import { FaTrashAlt } from "react-icons/fa";
 
 function Users() {
-    return (
-        <div><h1>My Classmates </h1></div>
-    )
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+
+  const handleRemove = (userId) => {
+    dispatch(removeUser(userId));
+  };
+
+  return (
+    <div className="users">
+      <h2>My Classmates</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} {user.lastname} - {user.email}
+            <button onClick={() => handleRemove(user.id)}>
+              <FaTrashAlt />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Users
+export default Users;
