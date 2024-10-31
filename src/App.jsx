fd0fd28d39e05990a2ea4c3881aff2ec6a0ee34e
meclozano <container.me@gmail.com>
2024-10-31 13:37:50 -0500

    pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/Layout";
 import PublicRoute from "./routes/PublicRoutes";
 import Login from "./pages/Login";
+import Home from "./pages/Home"; // Importa el componente Home
 
 function App() {
   return (
@@ -13,6 +14,14 @@ function App() {
             element={
               <PublicRoute>
                 <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/home" 
+            element={
+              <PublicRoute>
+                <Home />
               </PublicRoute>
             }
           />
