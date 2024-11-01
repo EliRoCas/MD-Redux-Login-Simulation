bfe18f8d6859528a172bbee13b145db214f78588
Eli RoCas <elir_94@hotmail.com>
2024-10-31 20:33:50 -0500

    Not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
+import "./notFound.scss";
+import notFound from "../assets/img/notFound.jpg";
 
 function NotFound() {
-    return (
-        <div>NotFound</div>
-    )
+  return (
+    <div className="notFound">
+      <img src={notFound} alt="Not Found" className="nfImg" />
+      <h2 className="nfText">Sorry! Something went wrong.</h2>
+      <Link to="/" className="nfLink">
+        Go back to Home
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
